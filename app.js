802f73ebb1c38a496e79b7bb6364b456993b0884
ptrszmczk <ptrszmczk@gmail.com>
2023-03-14 16:42:26 +0100

    fix: change in base url

--- app.js
+++ app.js
@@ -35,6 +35,11 @@ app.use(helmet());
 app.use(cors());
 app.use(xss());
 
+// base route for website
+app.get('/', (req, res) => {
+  res.send('<h1>Jobs API</h1><a href="/api/docs">Documentation</a>')
+});
+
 // routes
 app.use('/api/v1/auth', authRouter);
 app.use('/api/v1/jobs', authenticateUser, jobsRouter);
